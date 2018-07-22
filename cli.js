e4b76e783b3a09b688213f562325f82cf0cb8458
Ajay Narain Mathur <amathur@atlassian.com>
2018-07-22 12:15:46 +1000

    working encoding with a bug

--- cli.js
+++ cli.js
@@ -1,39 +1,27 @@
 #!/usr/bin/env node
 // @flow
 const path = require("path");
-const { ensureFileExits, readFile } = require("./utils/fs");
+const { encode, decode } = require("./lib/operation");
+const { ensureFileExits, readFile, writeFile } = require("./lib/utils/fs");
 
-/*::
-  type Node = {
-    id: number,
-    left: number,
-    right: number,
-    type: 'INTERNAL_NODE' | 'LEAF_NODE',
-    weight: number,
-    value: ?string,
-  }
-*/
-
-async function cli(args /*: Array<string> */) {
-  const fileToCompress = args.pop();
-  const pathToFile = path.join(process.cwd(), fileToCompress);
-
+async function cli(argv) {
+  const cwd = process.cwd();
+  const [command, fileName] = argv;
+  const pathToFile = path.join(process.cwd(), fileName);
   const FILE_EXISTS = await ensureFileExits(pathToFile);
-
   if (!FILE_EXISTS) {
     console.log(`ERROR: ${pathToFile} does not exits on disk`);
     process.exit(1);
   }
 
-  const FILE_CONTENT = await readFile(pathToFile, "utf8");
-
-  let ocurrancesTable = new Map();
-
-  for (let i = 0; i < FILE_CONTENT.length; i++) {
-    let currentOccurence = ocurrancesTable.get(FILE_CONTENT[i]) || 0;
-    ocurrancesTable.set(FILE_CONTENT[i], ++currentOccurence);
+  if (command === "encode") {
+    await encode(pathToFile, cwd);
+  } else if (command === "decode") {
+    await decode(pathToFile, cwd);
+  } else {
+    console.log(`ERROR: ${command} is not a supported operation`);
+    process.exit(1);
   }
-  console.log({ ocurrancesTable });
 }
 
 cli(process.argv.slice(2)).then(() => {
